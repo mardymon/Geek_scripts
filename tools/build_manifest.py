be711490651b50c6ff24dee31c6d5cff71b3b76f
mardymon <nikita.myalik@gmail.com>
2025-09-05 19:54:52 +0300

    Update build_manifest.py

--- tools/build_manifest.py
+++ tools/build_manifest.py
@@ -20,7 +20,7 @@ PACKAGES = [
     {
         "id": "rename-artboards",
         "name": "Rename Artboards",
-        "version": "1.0.1",
+        "version": "1.0.2",
         "description": "Переименование артбордов по шаблону.",
         "src_dir": "scripts/rename-artboards",
         "dest_subdir": ""
