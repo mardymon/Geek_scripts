aabfb8336bc6675ac9e85a7e817c6652eef1d560
mardymon <nikita.myalik@gmail.com>
2025-09-12 17:35:03 +0300

    manifest: pin @6b0578b (rename-artboards 1.0.7)

--- tools/build_manifest.py
+++ tools/build_manifest.py
@@ -25,7 +25,7 @@ PACKAGES = [
     {
         "id": "rename-artboards",
         "name": "Rename Artboards",
-        "version": "1.0.6",  # ← ставь здесь актуальную версию пакета
+        "version": "1.0.7",  # ← ставь здесь актуальную версию пакета
         "description": "Переименование артбордов по шаблону.",
         "src_dir": "scripts/rename-artboards",
         "dest_subdir": ""
