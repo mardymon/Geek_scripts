4e72365765f0deb96f23ba0f9e951e2b72c56a8e
mardymon <nikita.myalik@gmail.com>
2026-01-30 15:58:43 +0500

    Update build_manifest.py

--- tools/build_manifest.py
+++ tools/build_manifest.py
@@ -15,7 +15,7 @@ PACKAGES = [
     {
         "id": "1 Кол-во",
         "name": "1 Кол-во",
-        "version": "1.0.2",
+        "version": "1.0.3",
         "description": "Создание копий макетов",
         "src_dir": "scripts/1 Кол-во",
         "dest_subdir": "",
@@ -33,7 +33,7 @@ PACKAGES = [
     {
         "id": "3 Name лист",
         "name": "3 Name лист",
-        "version": "1.0.1",
+        "version": "1.0.2",
         "description": "Подписать лист печати",
         "src_dir": "scripts/3 Name лист",
         "dest_subdir": "",
@@ -42,7 +42,7 @@ PACKAGES = [
     {
         "id": "4 Артикул под меткой",
         "name": "4 Артикул под меткой",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "description": "Подписать артикул под меткой печати",
         "src_dir": "scripts/4 Артикул под меткой",
         "dest_subdir": "",
@@ -51,7 +51,7 @@ PACKAGES = [
     {
         "id": "5 Артикул слева",
         "name": "5 Артикул слева",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "description": "Подписать артикул для листа печати",
         "src_dir": "scripts/5 Артикул слева",
         "dest_subdir": "",
@@ -105,7 +105,7 @@ PACKAGES = [
     {
         "id": "93 Артикул под макетом",
         "name": "93 Артикул под макетом",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "description": "Подписать артикул у макета",
         "src_dir": "scripts/93 Артикул под макетом",
         "dest_subdir": "",
@@ -132,7 +132,7 @@ PACKAGES = [
     {
         "id": "96 Штрих-код контура",
         "name": "96 Штрих-код контура",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "description": "Выгрузить и создать штрих-код контура",
         "src_dir": "scripts/96 Штрих-код контура",
         "dest_subdir": "",
